5a57a03f70d122fde0902b20f4662a8c1df0c2b4
fraolb <Fraolbereket@gmail.com>
2023-05-26 19:47:26 +0300

    math

--- src/scientificCalculator.js
+++ src/scientificCalculator.js
@@ -1,18 +1,30 @@
 // Return the ceil value of the number, check if the number is < or = zero, then throw an Error
 const computeCeil = (number) => {
-  
+  if(number <= 0){
+    throw Error("ERROR::Number is less than or equal to zero..!")
+  }
+  return Math.ceil(number)
 }
 // Return the floor value of the number, check if the number is < or = zero, then throw an Error
 const computeFloor = (number) => {
-  
+  if(number <= 0){
+    throw Error("ERROR::Number is less than or equal to zero..!")
+  }
+  return Math.floor(number)
 }
 // Return the square root of the number, check if the number is < or = zero, then throw an Error
 const computeSquareRoot = (number) => {
-  
+  if(number <= 0){
+    throw Error("ERROR::Number is less than or equal to zero..!")
+  }
+  return Math.sqrt(number)
 }
 // Return the exponent value of the number, check if the number is < or = zero, then throw an Error
 const computePower = (number, powerOf) => {
-  
+  if(number <= 0 || powerOf <=0){
+    throw Error("ERROR::Number or power is less than or equal to zero")
+  }
+  return Math.pow(number, powerOf)
 }
 
 module.exports = {

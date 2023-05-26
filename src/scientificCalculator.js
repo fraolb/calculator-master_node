7284dfcd1c8a3ec8b4db8aeb484790f84a9fbd1c
fraolb <Fraolbereket@gmail.com>
2023-05-26 18:12:13 +0300

    calc_with error handling initial

---
+++ src/scientificCalculator.js
@@ -0,0 +1,20 @@
+// Return the ceil value of the number, check if the number is < or = zero, then throw an Error
+const computeCeil = (number) => {
+  
+}
+// Return the floor value of the number, check if the number is < or = zero, then throw an Error
+const computeFloor = (number) => {
+  
+}
+// Return the square root of the number, check if the number is < or = zero, then throw an Error
+const computeSquareRoot = (number) => {
+  
+}
+// Return the exponent value of the number, check if the number is < or = zero, then throw an Error
+const computePower = (number, powerOf) => {
+  
+}
+
+module.exports = {
+  computeCeil, computeFloor, computeSquareRoot, computePower
+}
